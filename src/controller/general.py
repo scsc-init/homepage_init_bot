530696030593db8fd9de17eb14cb1cd6f8aadae8
1n1tial <dan.yun0821@gmail.com>
2025-07-05 12:42:32 +0900

    feat: add embed

--- src/controller/general.py
+++ src/controller/general.py
@@ -1,3 +1,5 @@
+import discord
+
 from src.bot.discord import SCSCBotConnector
 
 async def create_invite(connector: SCSCBotConnector, body: dict):
@@ -5,9 +7,21 @@ async def create_invite(connector: SCSCBotConnector, body: dict):
     return {"invite_url": str(invite)}
 
 async def send_message_by_id(connector: SCSCBotConnector, body: dict):
+    if body['embed']: 
+        embed = discord.Embed(title=body['embed'].get("title"), description=body['embed'].get("description"), color=discord.Color(body['embed'].get("color")))
+        for field in body['embed'].get("fields", []):
+            embed.add_field(name=field.get("name"), value=field.get("value"),inline=field.get("inline", True))
+        connector.send_message(body['channel_id'], body['content'], embed)
+        return
     connector.send_message(body['channel_id'], body['content'])
     
 async def send_message_by_name(connector: SCSCBotConnector, body: dict):
+    if body['embed']: 
+        embed = discord.Embed(title=body['embed'].get("title"), description=body['embed'].get("description"), color=discord.Color(body['embed'].get("color")))
+        for field in body['embed'].get("fields", []):
+            embed.add_field(name=field.get("name"), value=field.get("value"),inline=field.get("inline", True))
+        connector.send_message(body['channel_name'], body['content'], embed)
+        return
     connector.send_message(body['channel_name'], body['content'])
     
 async def get_id_from_name(connector: SCSCBotConnector, body: dict):
