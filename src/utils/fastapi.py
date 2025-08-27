4275e4d0ce133b82fcae8868d05aed24be32916c
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2025-08-27 16:33:28 +0900

    refactor/login

--- src/utils/fastapi.py
+++ src/utils/fastapi.py
@@ -10,28 +10,32 @@ r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 LOGIN_KEY = "bot:jwt"
 
-@app.get("/status")
-async def status():
+
+async def get_logged_in() -> bool:
     async with httpx.AsyncClient() as client:
         jwt = r.get(LOGIN_KEY)
-        if not jwt: 
-            return {"logged_in": False}
+        if not jwt: return False
         res = await client.get(f"http://{get_settings().main_backend_host}:8080/api/user/profile", headers={"x-api-secret": get_settings().api_secret, "x-jwt": jwt})
-        if res.status_code != 200: return {"logged_in": False}
-        else: return {"logged_in": True}
-            
+        if res.status_code != 200: return False
+        return True
+
+
+@app.get("/status")
+async def status():
+    return {"logged_in": await get_logged_in()}
+
 
 @app.post("/login", status_code=204)
 async def login():
+    if await get_logged_in(): return
     async with httpx.AsyncClient() as client:
         res = await client.post(f"http://{get_settings().main_backend_host}:8080/api/user/login", headers={"x-api-secret": get_settings().api_secret}, json={"email": "bot@discord.com"})
 
         if res.status_code == 200:
             token = res.json().get("jwt")
             r.set(LOGIN_KEY, token)
-        else:
-            raise HTTPException(400, detail=res.text)
-        
-        
+        else: raise HTTPException(400, detail=res.text)
+
+
 def logout():
     r.delete(LOGIN_KEY)
